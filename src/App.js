d102a673e8acdc1fbb839cc39378d71a8f93554a
Eduardo Dallabella Lima <eduardo.lima@fiap.com.br>
2024-12-21 23:45:32 -0300

    Adicionando projeto Organo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+
+function App() {
+
+  const [colaboradores, setColaboradores] = useState('')
+
+  const aoNovoColaboradorAdicionar = ( colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionar(colaborador)}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header class="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"></img>
+        </header>
+    )
+}
+
+export default Banner;
